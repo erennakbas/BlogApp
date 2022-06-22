5a5c7358bcce236d3b8a4e0b26f85289269290b8
theroxz <ahmet.eren4219@hotmail.com>
2022-06-22 13:20:11 +0300

    .

--- blogapp/blogapp/settings.py
+++ blogapp/blogapp/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
+env = environ.Env()
+environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(d(n3^kb2ew6)f$nzg9+ohlydp*k(@y3_28&ybf*n@3i39g0c%'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
